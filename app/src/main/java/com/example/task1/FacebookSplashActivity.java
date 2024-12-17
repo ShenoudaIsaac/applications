2eab4051dd8a03b61215963849ee7ee1479a7188
ShenoudaIsaac <shenoudaisaac54@gmail.com>
2024-12-17 10:51:44 +0200

    first task

---
+++ app/src/main/java/com/example/task1/FacebookSplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.task1;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class FacebookSplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_facebook);
+
+
+
+
+    }
+}
